a288a5fcf17b5e0416ec2dcf9e503b7b3549235a
Nassim Marhari <92116345+nmarhari@users.noreply.github.com>
2022-09-29 13:31:21 -0400

    Update studymaterial.cpp

--- studymaterial.cpp
+++ studymaterial.cpp
@@ -77,7 +77,11 @@ this is not finished yet
 			// this will only return a result
 			// const methods can NOT modify object it is being called upon
 			// in this case it is being called upon a so a will not be modified.
-			
+		5. You can return a reference in a function/method.
+			// ex: string& string::operator+=(const string& rhs) {...}
+				//  ^^^ 
+			// you can not return local variables by reference
+			// you can not return pointers to local variables
 			
 	L values and R values
 	
@@ -91,7 +95,12 @@ this is not finished yet
 			// 20 is r-value because how tf you gonna modify 20?
 			
 		3. All literals and const objects are r-values.
-		
+			// literals are values we write in conventional form whose value is obvious
+			// or	fixed values that the program may NOT alter
+			// example:
+				const int = 5;
+				// 5 is a literal. you cant modify 5. if you can im an idiot
+				
 	
 	This
 	
@@ -99,6 +108,41 @@ this is not finished yet
 		2. This // is a pointer
 		3. This // points to the object that a method is being invoked upon
 			
+	
+	Pointers
+	
+		1. Pointer is a variable that holds a memory address
+			// pointers allow us to modify or manipulate variables using
+			// memory addresses through reference and dereference.
+			// it can allow us to manipulate several memory locations because
+			// the memory address that is stored by the pointer can be changed
+		2. Address of / reference operator:  &
+		3. dereference operator:	     *
+			// example:
+				int x = 10;
+				int *ptr; 
+				ptr = &x; // ptr references x	  or	ptr = the address of x 
+				*ptr = 20; // set what ptr points to, to 20
+				
+			// you can also do
+				ptr = 0; // ptr points to nothing (null)
+				ptr = nullptr; // in certain programs also set it to (null)
+		4. Reference variables... // are actually pointers with some syntactic sugar
+			// example:
+				int a;
+				int &ref = a; // ref vars must have initial value
+					      // ref vars can NOT change what they point to
+				ref = 20;     // a is now 20.
+			// // // // // // // // // // // // // // // // //
+				int *ptr = &a;
+				*ptr = 30;    // a is now 30.
+			// this ^^^^ does the same thing as the &ref example.
+			
+
+	Dynamic Array Allocation
+		
+		1. ......
+
 }
 
 
@@ -152,6 +196,23 @@ this is not finished yet
 			// if you declare an array of a type which doesnt have default constructor
 			// you will get an error. default const is called for each element of array.
 			// default constructor can do nothing
+
+	Memory
+	
+		1. Is a sequential list of locations (words)
+		2. Each word in memory has an address (ex: FF01) // can not directly address a byte or bit
+			// address typically given in hexadecimal (FF01). every 4 bits is one hex digit
+			// 1 byte is 2 hex digits.
+			// hex is faster and easier than binary addresses. (0xFF01 = 0b1111111100000001)
+			// "0x" indicates that the following number will be in hex
+			// hex is base 16 (0-9 and A-F), binary is base 2 (0, 1), decimal is base 10 (0-9)
+		3. 1 byte = 8 bits
+		4. int = 1 word in size... // words are made of one or more bytes. 1 bit is for (-) or (+)
+		5. char = 1 byte // (= 8 bits) BUT a whole word is allocated.
+		6. Memory is allocated in blocks // int[10] is 10 CONSECUTIVE words in memory
+						 // char[10] is 10 CONSECUTIVE bytes
+		7. Class with 4 int is 4 CONSECUTIVE words in memory
+		8. "https://openframeworks.cc/ofBook/chapters/memory.html"
 
 
 	C++ Compilation 		// no not like a call of duty trickshot compilation
